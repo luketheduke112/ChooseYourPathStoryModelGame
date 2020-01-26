54733ced0618230b750ff2289e1ab99ff8e9fd7a
luketheduke112 <lukedavidfisher03@gmail.com>
2020-01-26 16:44:36 -0500

    prototyped player stats and characteristics

---
+++ Subsystems/PlayerCharacteristics.java
@@ -0,0 +1,9 @@
+/**
+ * PlayerCharacteristics
+ */
+import java.util.ArrayList;
+public class PlayerCharacteristics {
+    public static int choicesMade = 0;
+    public static ArrayList<String> playStyle = new ArrayList<String>();
+    
+}
