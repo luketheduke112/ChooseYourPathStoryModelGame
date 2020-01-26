7a56cd3009757adcc803f6ae42c1f1e7fe5f5f90
luketheduke112 <lukedavidfisher03@gmail.com>
2020-01-26 16:44:18 -0500

    built story content

--- Subsystems/IndividualStoryContent.java
+++ Subsystems/IndividualStoryContent.java
@@ -2,6 +2,16 @@
  * IndividualStoryContent
  */
 public class IndividualStoryContent {
+    String mainBodyContent;
+    String scenarioTitle;
+    
+    IndividualStoryContent(String mainBodyContent, String scenarioTitle) {
+        this.mainBodyContent = mainBodyContent;
+        this.scenarioTitle = scenarioTitle;
+    }
 
+    public void printOutBody() {
+        System.out.println(mainBodyContent);
+    }
     
 }
